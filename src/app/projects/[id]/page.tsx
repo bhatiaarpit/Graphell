a37e59f14da664da505e21524012f33283318f98
Arpit Bhatia <arpitbhatia903@gmail.com>
2025-05-09 13:28:16 +0530

    ..

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -22,7 +22,7 @@ const Project = ({ params }: Props) => {
       <ModalNewTask
         isOpen={isModalNewTaskOpen}
         onClose={() => setIsModalNewTaskOpen(false)}
-        id={id}
+        id={"id"}
       />
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
